e1e6fa299ab989e3555a3cd9f994821cf8b0bf6b
Tetiana Ruzhetska <tetiana.ruzhetska@gmail.com>
2023-06-11 01:21:26 +0300

    update home

--- Screens/Home.js
+++ Screens/Home.js
@@ -28,130 +28,7 @@ export const Home = () => {
 
   return (
     <NavigationContainer>
-      <Tabs.Navigator
-        screenOptions={{
-          tabBarShowLabel: false,
-          headerTitleAlign: "center",
-          initialRouteName: "PostsScreen",
-        }}
-      >
-        <Tabs.Screen
-          name="Posts"
-          component={PostsScreen}
-          options={{
-            tabBarIcon: () => (
-              <Image
-                source={require("../assets/images/grid.png")}
-                style={{ width: 40, height: 40 }}
-              ></Image>
-            ),
-            headerRight: () => (
-              <TouchableOpacity onPress={() => dispatch(logout())}>
-                <Image
-                  source={require("../assets/images/logout.png")}
-                  style={{ width: 24, height: 24, marginRight: 16 }}
-                ></Image>
-              </TouchableOpacity>
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="Create Post"
-          component={CreatePostsScreen}
-          options={({ navigation }) => ({
-            tabBarIcon: () => (
-              <View style={styles.addPostBtn}>
-                <Image
-                  source={require("../assets/images/union.png")}
-                  style={{ width: 13, height: 13 }}
-                ></Image>
-              </View>
-            ),
-            headerLeft: () => (
-              <TouchableOpacity onPress={() => navigation.navigate("Posts")}>
-                <Image
-                  source={require("../assets/images/arrowleft.png")}
-                  style={{
-                    width: 24,
-                    height: 24,
-                    marginRight: 16,
-                    marginLeft: 16,
-                  }}
-                ></Image>
-              </TouchableOpacity>
-            ),
-            tabBarStyle: {
-              display: "none",
-            },
-          })}
-        />
-        <Tabs.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={({ navigation }) => ({
-            tabBarIcon: () => (
-              <Image
-                source={require("../assets/images/user.png")}
-                style={{ width: 40, height: 40 }}
-              ></Image>
-            ),
-            headerLeft: () => (
-              <TouchableOpacity onPress={() => navigation.navigate("Posts")}>
-                <Image
-                  source={require("../assets/images/arrowleft.png")}
-                  style={{
-                    width: 24,
-                    height: 24,
-                    marginRight: 16,
-                    marginLeft: 16,
-                  }}
-                ></Image>
-              </TouchableOpacity>
-            ),
-          })}
-        />
-        <Tabs.Screen
-          name="Comments"
-          component={CommentsScreen}
-          options={({ navigation }) => ({
-            tabBarButton: () => null,
-            headerLeft: () => (
-              <TouchableOpacity onPress={() => navigation.navigate("Posts")}>
-                <Image
-                  source={require("../assets/images/arrowleft.png")}
-                  style={{
-                    width: 24,
-                    height: 24,
-                    marginRight: 16,
-                    marginLeft: 16,
-                  }}
-                ></Image>
-              </TouchableOpacity>
-            ),
-          })}
-        />
-        <Tabs.Screen
-          name="Map"
-          component={MapScreen}
-          options={({ navigation }) => ({
-            tabBarButton: () => null,
-            headerLeft: () => (
-              <TouchableOpacity onPress={() => navigation.navigate("Posts")}>
-                <Image
-                  source={require("../assets/images/arrowleft.png")}
-                  style={{
-                    width: 24,
-                    height: 24,
-                    marginRight: 16,
-                    marginLeft: 16,
-                  }}
-                ></Image>
-              </TouchableOpacity>
-            ),
-          })}
-        />
-      </Tabs.Navigator>
-      {/* {isAuth ? (
+      {isAuth ? (
         <Tabs.Navigator
           screenOptions={{
             tabBarShowLabel: false,
@@ -287,7 +164,7 @@ export const Home = () => {
             component={RegistrationScreen}
           />
         </MainStack.Navigator>
-      )} */}
+      )}
     </NavigationContainer>
   );
 };
